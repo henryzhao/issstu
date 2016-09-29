7419f5a122966bd737f6d20d00483d1bfeaa43ab
zhaoyuqi <zhaoyuqi54@gmail.com>
2016-09-29 10:29:10 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name issstuApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the issstuApp
+ */
+angular.module('issstuApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name issstuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the issstuApp
+ */
+angular.module('issstuApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
